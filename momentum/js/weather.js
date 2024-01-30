d1bf7276bed8f4d64abc03778e94e34535dbfc56
imyeyun <98500675+imyeyun@users.noreply.github.com>
2024-01-30 17:09:25 +0900

    Add files via upload

---
+++ momentum/js/weather.js
@@ -0,0 +1,17 @@
+const API_KEY ="53c8a3c7700b8b529deac9d34468ac87";
+function onGeoOk(position){
+    const lat=position.coords.latitude;
+    const lon=position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url).then(Response=>Response.json()).then(data=>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText =data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoerror(){
+    alert("Can't find you. No weather for you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoerror);
